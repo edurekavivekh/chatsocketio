f7bcdb2c1570b7edb503d0a1544a6f0c77dba721
root <root@ip-10-0-54-169.us-west-2.compute.internal>
2018-09-10 11:49:35 +0000

    test cases

--- test/test.js
+++ test/test.js
@@ -1,22 +1,12 @@
 var http = require('http');
 var assert = require('assert');
 
-var server = require('./server.js');
 
 describe('HTTP Server Test', function() {
 	
-	before(function() {
-		server.listen(8989);
-	});
-
-	
-	after(function() {
-		server.close();
-	});
-
 	describe('/', function() {
 		it('should be nickname!', function(done) {
-			http.get('http://127.0.0.1:8989', function(response) {
+			http.get('http://127.0.0.1:3000', function(response) {
 				
 				assert.equal(response.statusCode, 200);
 
